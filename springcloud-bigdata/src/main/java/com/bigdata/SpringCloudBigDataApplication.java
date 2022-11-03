79036fae170efa0bf2ca73357b0e1ba0a905303e
admin <mooncharmzx@163.com>
2022-11-03 11:29:38 +0800

    修改

--- springcloud-bigdata/src/main/java/com/bigdata/SpringCloudBigDataApplication.java
+++ springcloud-bigdata/src/main/java/com/bigdata/SpringCloudBigDataApplication.java
@@ -3,12 +3,8 @@ package com.bigdata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
-@SpringBootApplication
-class SpringCloudBigDataApplication {
+@SpringBootApplication(scanBasePackages = {"com.bigdata.*"})
+public class SpringCloudBigDataApplication {
 
     public static void main( String[] args ){
         SpringApplication.run(SpringCloudBigDataApplication.class, args);
